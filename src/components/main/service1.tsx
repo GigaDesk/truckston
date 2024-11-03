591c81a86ccb86879880ec906040fa932341f520
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-11-03 11:01:27 +0300

    feat(main): add main component

---
+++ src/components/main/service1.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export default function Service1(props) {
+  return (
+    <div
+      style={{ fontFamily: "Graphik" }}
+      className=" text-xl text-white bg-neutral-950 font-medium pt-20 grid md:grid-cols-2 gap-20"
+    >
+      <div>
+        <video width="720" height="480" autoPlay muted src={props.video} loop />
+      </div>
+      <div className="">
+        <hr className="w-8 h-1 bg-purple-950 border-0" />
+        <h3 className="py-6"> {props.name} </h3>
+        <p className="text-base font-normal">{props.description}</p>
+      </div>
+    </div>
+  );
+}
