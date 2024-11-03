fc52ec561f4ebb38f78a6ff58d16f48e6ab0e464
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-11-03 11:50:35 +0300

    fix: repair main and footer addition to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import Header from "../components/header";
+import Main from "../components/main";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <div className="fixed top-0 left-0 right-0">
         <Header />
       </div>
+      <Main />
+      <Footer />
     </div>
   );
 }
